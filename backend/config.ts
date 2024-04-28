89091ad2676c331b4b21ca939d015ace04b1979d
big dick bandit <sysop777@proton.me>
2024-04-28 09:27:37 -0400

    !!!

--- backend/config.ts
+++ backend/config.ts
@@ -1,8 +1,8 @@
 export const config = {
-  mailcowAPIKey: "2D5592-CA5C06-8B6231-9C10D8-B31BC5",
-  mailcowBaseURL: "https://mx.footjobs.today",
-  imapServer: "mx.footjobs.today",
-  domain: "footjobs.today",
+  mailcowAPIKey: "2833E3-2E0327-0494DF-69F03B-061A98",
+  mailcowBaseURL: "https://mail.as393577.net",
+  imapServer: "mail.as393577.net",
+  domain: "pain.network",
   imapPort: "143",
   port: 4000,
 };
