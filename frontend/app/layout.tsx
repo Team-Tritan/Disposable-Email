ddd0f06d486c6b16e7bd79f9d6fbecf6d2a5f62e
big dick bandit <sysop777@proton.me>
2024-04-30 11:53:30 -0400

    !!

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-
 import { ToastContainer } from "react-toastify";
+import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import PlausibleProvider from "next-plausible";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,32 +18,36 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="https://s3.tritan.gg/main/tritan-bot/logo.webp"
+    <PlausibleProvider
+      domain="temp-mail.tritan.gg"
+      customDomain="https://analytics.tritan.gg"
+      selfHosted={true}
+      trackLocalhost={true}
+      trackOutboundLinks={true}
+      enabled={true}
+    >
+      <html lang="en">
+        <head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="https://s3.tritan.gg/main/tritan-bot/logo.webp"
+          />
+        </head>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
         />
-        <script
-          defer
-          data-domain="temp-mail.tritan.gg"
-          src="https://analytics.tritan.gg/js/script.js"
-        ></script>
-      </head>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <body className={inter.className}>{children}</body>
-    </html>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </PlausibleProvider>
   );
 }
