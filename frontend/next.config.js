ba6d7936e5927a918d43daa39cfbb90ab05dce2c
big dick bandit <sysop777@proton.me>
2024-04-30 17:45:51 -0400

    Update API destination URL in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination: "https://temp-mail-api.tritan.gg/api/:path*",
+        //destination: "http://localhost:4000/api/:path*",
       },
     ];
   },
